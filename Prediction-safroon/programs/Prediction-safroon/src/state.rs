e8d28511942aa1d597fdb0130909ed208221a898
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-02-21 17:48:14 +0530

    add the Beting PredictionAccount

--- Prediction-safroon/programs/Prediction-safroon/src/state.rs
+++ Prediction-safroon/programs/Prediction-safroon/src/state.rs
@@ -5,3 +5,13 @@ pub struct Master {
     // Master Account hold the programe and the master account itself
     pub last_bet_id: u64,
 }
+
+#[account]
+pub struct Bet {
+    pub id: u64,     // new ID
+    pub amount: u64, // Beting amount wroth
+    pub prediction_a: BetingPrediction,
+    pub prediction_b: Option<BetingPrediction>, // an Option
+    pub state: BetState,
+    pub pyth_price_key: Pubkey,
+}
