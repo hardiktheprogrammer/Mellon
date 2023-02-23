24ae8b3bc2b63847a31989bdb525f20e094060a3
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-02-23 21:42:37 +0530

    add Dependencs and deploy the contract

--- Prediction-safroon/programs/Prediction-safroon/src/state.rs
+++ Prediction-safroon/programs/Prediction-safroon/src/state.rs
@@ -8,10 +8,29 @@ pub struct Master {
 
 #[account]
 pub struct Bet {
-    pub id: u64,     // new ID
-    pub amount: u64, // Beting amount wroth
-    pub prediction_a: BetingPrediction,
-    pub prediction_b: Option<BetingPrediction>, // an Option
+    pub id: u64,                                 // new ID
+    pub amount: u64,                             // Beting amount wroth
+    pub prediction_a: BettingPrediction,         // take prediction from the bet creator
+    pub prediction_b: Option<BettingPrediction>, // an Option
+    pub expire_ts: i64,                          // expiration time of the bet that has beee created
     pub state: BetState,
-    pub pyth_price_key: Pubkey,
+    pub pyth_price_key: Pubkey, // Bet becomes the price of the bet
+}
+
+#[derive(Clone, AnchorSerialize, AnchorDeserialize)]
+
+pub struct BettingPrediction {
+    pub player: Pubkey, // Player
+
+    pub price: f64, // Price of the bet that has been created
+}
+
+#[derive(Clone, AnchorDeserialize, AnchorSerialize, AnchorDeserialize)]
+
+pub enum BetState {
+    Created,
+    Expired,
+    Draw,
+    PlayerAWon,
+    PlayerBWon,
 }
